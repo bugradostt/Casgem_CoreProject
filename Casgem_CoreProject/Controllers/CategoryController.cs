5468c4a0c0ff375659cd2baeeb38517768abe4c3
budotekno <budotekno@gmail.com>
2023-07-13 14:51:47 +0300

    katmanlar tamamlandı

---
+++ Casgem_CoreProject/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.BusinessLayer.Abstract;
+using PizzaPan.BusinessLayer.Concrete;
+using PizzaPan.DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPan.PresentationLayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        readonly ICategoryService _categoryService;
+
+        CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        public IActionResult Index()
+        {
+            var values = _categoryService.TGetList();
+            return View(values);
+        }
+    }
+}
