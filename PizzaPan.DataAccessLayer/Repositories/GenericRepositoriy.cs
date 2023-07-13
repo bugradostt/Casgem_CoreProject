2822ada9be80555f8421e38b4e323f506c217f48
budotekno <budotekno@gmail.com>
2023-07-13 12:15:46 +0300

    add businessLayer

---
+++ PizzaPan.DataAccessLayer/Repositories/GenericRepositoriy.cs
@@ -0,0 +1,42 @@
+﻿using PizzaPan.DataAccessLayer.Abstract;
+using PizzaPan.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPan.DataAccessLayer.Repositories
+{
+    public class GenericRepositoriy<T> : IGenericDal<T> where T : class
+    {
+        Context c = new Context();
+        public void Delete(T t)
+        {
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return c.Set<T>().Find();
+        }
+
+        public List<T> GetList()
+        {
+            return c.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            c.Update(t);
+            c.SaveChanges();
+        }
+    }
+}
