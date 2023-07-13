9259202bcd575dcb449f348541e729c9ff0b8480
budotekno <budotekno@gmail.com>
2023-07-13 11:01:40 +0300

    add migration

--- PizzaPan.DataAccessLayer/Concrete/Context.cs
+++ PizzaPan.DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaPan.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,19 @@ using System.Threading.Tasks;
 
 namespace PizzaPan.DataAccessLayer.Concrete
 {
-    public class Context
+    public class Context : DbContext
     {
-        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=BUDOTEKNO\\SQLEXPRESS; initial catalog=Casgem_CoreProject; integrated security=true");
+
+        }
+
+        public DbSet<Category> Categories { get; private set; }
+        public DbSet<Contact> Contacts { get; private set; }
+        public DbSet<Product> Products { get; private set; }
+        public DbSet<Testimonial> Testimonials { get; private set; }
+
     }
 }
