3529ce0504b853443c410ebfa2ecedf0345200b7
bugradostt <bugradostt@gmail.com>
2023-07-21 09:48:56 +0300

    vitrin contact add

---
+++ Casgem_CoreProject/Controllers/VitrinContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaPan.PresentationLayer.Controllers
+{
+    public class VitrinContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
